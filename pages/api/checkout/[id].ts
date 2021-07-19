2e03cdd4cac70522ab5d25b558125904307446e2
Eapu <elena.andresprado@gmail.com>
2021-07-19 16:07:05 +0100

    fix error

--- pages/api/checkout/[id].ts
+++ pages/api/checkout/[id].ts
@@ -1,11 +1,11 @@
-import { NextApiRequest, NexApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2020-08-27'
 });
 
-export default async function handler(req:NextApiRequest, res:NexApiResponse) {
+export default async function handler(req:NextApiRequest, res:NextApiResponse) {
   const { id } = req.query;
   const session = await stripe.checkout.sessions.retrieve(id as string, { expand:['payment_intent'] })
 
