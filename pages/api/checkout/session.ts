41f77b282d9d918ef4861817abbd2a91525e7df0
Eapu <elena.andresprado@gmail.com>
2021-07-19 16:09:00 +0100

    fix error

--- pages/api/checkout/session.ts
+++ pages/api/checkout/session.ts
@@ -1,11 +1,11 @@
-import { NextApiRequest, NexApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2020-08-27'
 });
 
-export default async function handler(req:NextApiRequest, res:NexApiResponse) {
+export default async function handler(req:NextApiRequest, res:NextApiResponse) {
   const {quantity} = req.body;
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
