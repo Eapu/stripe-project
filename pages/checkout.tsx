7285da5dd183ec172dd6afad8989bba4e87cda23
Eapu <elena.andresprado@gmail.com>
2021-07-19 16:10:42 +0100

    fix error NextPage

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,4 +1,4 @@
-import { Nextpage } from 'next';
+import { NextPage } from 'next';
 import { loadStripe } from '@stripe/stripe-js';
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
